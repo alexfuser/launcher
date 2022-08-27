d142e85b5e538e04f2ae67cdc6b77602624d0c4c
Alex Fuser <alexfuser3@gmail.com>
2022-08-27 12:44:05 -0500

    Integracion funcional de launcher y kiosk

--- app/src/androidTest/java/com/vidar/applauncherdemo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/vidar/applauncherdemo/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("com.babybillssoftwarefactory.applauncherdemo", appContext.packageName)
+        assertEquals("com.vidar.applauncherdemo", appContext.packageName)
     }
 }
--- app/src/main/kotlin/com/vidar/applauncherdemo/view/activities/MainActivity.kt
+++ app/src/main/kotlin/com/vidar/applauncherdemo/view/activities/MainActivity.kt
@@ -9,6 +9,28 @@ import androidx.appcompat.app.AppCompatActivity
 import com.vidar.applauncherdemo.R
 import com.vidar.applauncherdemo.util.logging.Logger
 
+import android.app.PendingIntent
+import android.app.PendingIntent.FLAG_IMMUTABLE
+import android.app.admin.DevicePolicyManager
+import android.app.admin.SystemUpdatePolicy
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.content.IntentSender
+import android.content.pm.PackageInstaller
+import android.content.pm.PackageInstaller.SessionParams
+import android.os.BatteryManager
+import android.os.UserManager
+import android.provider.Settings
+import android.view.View
+import com.google.android.material.snackbar.Snackbar
+import com.vidar.applauncherdemo.MyDeviceAdminReceiver
+import com.vidar.applauncherdemo.databinding.ActivityMainBinding
+import kotlinx.android.synthetic.main.content_main.view.*
+
+//import com.vidar.applauncherdemo.databinding.ActivityMainBinding
+
 //endregion import directives
 
 
@@ -23,16 +45,167 @@ class MainActivity : AppCompatActivity() {
     /**
      * Called when the activity is created
      */
+
+    private lateinit var mAdminComponentName: ComponentName
+    private lateinit var mDevicePolicyManager: DevicePolicyManager
+    private lateinit var binding: ActivityMainBinding
+
+
+
+
+
+    companion object {
+        const val LOCK_ACTIVITY_KEY = "com.vidar.applauncherdemo.MainActivity"
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         try {
             // call base class implementation
             super.onCreate(savedInstanceState)
 
             // inflate layout
-            setContentView(R.layout.activity_main)
+            //setContentView(R.layout.activity_main)
+            //binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+            binding = ActivityMainBinding.inflate(layoutInflater)
+            setContentView(binding.root)
         } catch (ex: Exception) {
             Logger.w(ex)
         }
+        mAdminComponentName = MyDeviceAdminReceiver.getComponentName(this)
+        mDevicePolicyManager = getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
+
+        mDevicePolicyManager.removeActiveAdmin(mAdminComponentName)
+
+        val isAdmin = isAdmin()
+        if (isAdmin) {
+            Snackbar.make(binding.root, R.string.device_owner, Snackbar.LENGTH_SHORT).show()
+        } else {
+            Snackbar.make(binding.root, R.string.not_device_owner, Snackbar.LENGTH_SHORT).show()
+        }
+        binding.include.btStartLockTask.setOnClickListener {
+            setKioskPolicies(true, isAdmin)
+        }
+        binding.include.btStopLockTask.setOnClickListener {
+            setKioskPolicies(false, isAdmin)
+            val intent = Intent(applicationContext, MainActivity::class.java).apply {
+                addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            }
+            intent.putExtra(LOCK_ACTIVITY_KEY, false)
+            startActivity(intent)
+        }
+    }
+
+    private fun isAdmin() = mDevicePolicyManager.isDeviceOwnerApp(packageName)
+
+    private fun setKioskPolicies(enable: Boolean, isAdmin: Boolean) {
+        if (isAdmin) {
+            setRestrictions(enable)
+            enableStayOnWhilePluggedIn(enable)
+            setUpdatePolicy(enable)
+            setAsHomeApp(enable)
+            setKeyGuardEnabled(enable)
+        }
+        setLockTask(enable, isAdmin)
+        setImmersiveMode(enable)
+    }
+    private fun setRestrictions(disallow: Boolean) {
+        setUserRestriction(UserManager.DISALLOW_SAFE_BOOT, disallow)
+        setUserRestriction(UserManager.DISALLOW_FACTORY_RESET, disallow)
+        setUserRestriction(UserManager.DISALLOW_ADD_USER, disallow)
+        setUserRestriction(UserManager.DISALLOW_MOUNT_PHYSICAL_MEDIA, disallow)
+        setUserRestriction(UserManager.DISALLOW_ADJUST_VOLUME, disallow)
+        mDevicePolicyManager.setStatusBarDisabled(mAdminComponentName, disallow)
+    }
+
+    private fun setUserRestriction(restriction: String, disallow: Boolean) = if (disallow) {
+        mDevicePolicyManager.addUserRestriction(mAdminComponentName, restriction)
+    } else {
+        mDevicePolicyManager.clearUserRestriction(mAdminComponentName, restriction)
+    }
+    // endregion
+
+    private fun enableStayOnWhilePluggedIn(active: Boolean) = if (active) {
+        mDevicePolicyManager.setGlobalSetting(
+            mAdminComponentName,
+            Settings.Global.STAY_ON_WHILE_PLUGGED_IN,
+            (BatteryManager.BATTERY_PLUGGED_AC
+                    or BatteryManager.BATTERY_PLUGGED_USB
+                    or BatteryManager.BATTERY_PLUGGED_WIRELESS).toString()
+        )
+    } else {
+        mDevicePolicyManager.setGlobalSetting(mAdminComponentName, Settings.Global.STAY_ON_WHILE_PLUGGED_IN, "0")
+    }
+
+    private fun setLockTask(start: Boolean, isAdmin: Boolean) {
+        if (isAdmin) {
+            mDevicePolicyManager.setLockTaskPackages(
+                mAdminComponentName, if (start) arrayOf(packageName) else arrayOf()
+            )
+        }
+        if (start) {
+            startLockTask()
+        } else {
+            stopLockTask()
+        }
+    }
+    private fun setUpdatePolicy(enable: Boolean) {
+        if (enable) {
+            mDevicePolicyManager.setSystemUpdatePolicy(
+                mAdminComponentName,
+                SystemUpdatePolicy.createWindowedInstallPolicy(60, 120)
+            )
+        } else {
+            mDevicePolicyManager.setSystemUpdatePolicy(mAdminComponentName, null)
+        }
+    }
+
+    private fun setAsHomeApp(enable: Boolean) {
+        if (enable) {
+            val intentFilter = IntentFilter(Intent.ACTION_MAIN).apply {
+                addCategory(Intent.CATEGORY_HOME)
+                addCategory(Intent.CATEGORY_DEFAULT)
+            }
+            mDevicePolicyManager.addPersistentPreferredActivity(
+                mAdminComponentName, intentFilter, ComponentName(packageName, MainActivity::class.java.name)
+            )
+        } else {
+            mDevicePolicyManager.clearPackagePersistentPreferredActivities(
+                mAdminComponentName, packageName
+            )
+        }
+    }
+    private fun setKeyGuardEnabled(enable: Boolean) {
+        mDevicePolicyManager.setKeyguardDisabled(mAdminComponentName, !enable)
+    }
+
+    private fun setImmersiveMode(enable: Boolean) {
+        if (enable) {
+            val flags = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_FULLSCREEN
+                    or View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY)
+            window.decorView.systemUiVisibility = flags
+        } else {
+            val flags = (View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                    or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                    or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN)
+            window.decorView.systemUiVisibility = flags
+        }
+    }
+
+    private fun createIntentSender(context: Context?, sessionId: Int, packageName: String?): IntentSender? {
+        val intent = Intent("INSTALL_COMPLETE")
+        if (packageName != null) {
+            intent.putExtra("PACKAGE_NAME", packageName)
+        }
+        val pendingIntent = PendingIntent.getBroadcast(
+            context,
+            sessionId,
+            intent,
+            FLAG_IMMUTABLE
+        )
+        return pendingIntent.intentSender
     }
     //endregion lifecycle overrides
 }
